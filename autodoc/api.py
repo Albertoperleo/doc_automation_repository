661d28169a90e11b7faa3dc19dd360acc2805d03
Albertoperleo <albperleo@alum.us.es>
2023-02-24 08:36:13 +0100

    feat: Commit entity and its retrieving functionality
    Implemented at Commit.py and modified pi.py to retrieve all commits data in repo.
    
    Fixes #5

--- autodoc/api.py
+++ autodoc/api.py
@@ -1,6 +1,6 @@
 from typing import List, TypeVar
 from octokit import Octokit
-from .models import *
+from autodoc.models import *
 import json
 
 T = TypeVar('T')
@@ -22,7 +22,13 @@ def api_github(octokit: Octokit, url: str, front_path: str = '', back_path: str 
     return json.loads(data_dump)
 
 def parse_url(url: str, front_path: str, back_path: str) -> str:
-    """Parses the url to the api endpoint
+    """Parses the url to the api endpoint. If it contains curved braces '{}' means
+    that we have an optional path we have to control.
+    
+    Attention:
+        - front_path and back_path: you must add the '/' inside the path. For example:
+            - front_path: 'git/'
+            - back_path: '/git'
 
     Args:
         url (str): initial url
@@ -32,9 +38,10 @@ def parse_url(url: str, front_path: str, back_path: str) -> str:
     Returns:
         str: endpoint
     """
-    dotcom_index = url.rfind(".com")
-    url_modified = url[dotcom_index + 4:]
-    return f"GET {front_path}{url_modified}{back_path}"
+    initial_index = url.rfind("github.com")
+    final_index = url.rfind("{") if "{" in url else -1
+    basic_url = url[initial_index + 10 : final_index]
+    return f"GET {front_path}{basic_url}{back_path}"
     
 
 
@@ -58,13 +65,11 @@ def get_repository(octokit: Octokit, url: str) -> Repository:
     contributors_url = repository_json['contributors_url']
     repository.contributors = get_contributors(octokit, contributors_url)
     
-    '''TODO
-    Retrieve repository commits
-    '''
+    commits_url = repository_json['commits_url']
+    repository.commits = get_commits(octokit, commits_url)
     
-    '''TODO
-    Retrieve repository issues
-    '''
+    issues_url = repository_json['issues_url']
+    repository.issues = get_issues(octokit, issues_url)
     
     print(repository)
     return repository
@@ -83,13 +88,26 @@ def get_contributors(octokit: Octokit, url: str) -> List[Contributor]:
     return Contributor.createContributors(contributors_json)
     
 def get_commits(octokit: Octokit, url: str) -> List[Commit]:
-    """TODO
-    Def retrieve commits function
+    """Creates the repository's commits list
+
+    Args:
+        octokit (Octokit): lib to retrieve data
+        url (str): link to commits
+
+    Returns:
+        List[Commit]: commits list
     """
-    pass
+    commits_json = api_github(octokit, url, front_path='', back_path='')
+    return Commit.createCommits(commits_json)
     
 def get_issues(octokit: Octokit, url: str) -> List[Commit]:
-    """TODO
-    Def retrieve issues function
+    """Creates the repository's issues list
+
+    Args:
+        octokit (Octokit): lib to retrieve data
+        url (str): link to issues
+
+    Returns:
+        List[Commit]: issues list
     """
-    pass
+    issues_json = api_github(octokit, url, front_path='', back_path='')
--- autodoc/models/Commit.py
+++ autodoc/models/Commit.py
@@ -1,3 +1,28 @@
+from __future__ import annotations
+from typing import List
+import json
+from autodoc.models import Contributor
+
+
 class Commit:
-    def __init__(self, commits_url) -> None:
-        pass
+    def __init__(self, 
+                 sha: str,
+                 author: str,
+                 date: str,
+                 message: str) -> None:
+        self.sha = sha
+        self.author = author
+        self.date = date
+        self.message = message
+        
+    @staticmethod
+    def create(commit_json: json) -> Commit:
+        sha = commit_json['sha']
+        author = commit_json['author']['login']
+        date = commit_json['commit']['author']['date']
+        message = commit_json['commit']['message']
+        return Commit(sha, author, date, message)
+    
+    @staticmethod
+    def createCommits(commits_json: json) -> List[Commit]:
+        return [Commit.create(c) for c in commits_json]
